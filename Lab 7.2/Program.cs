c1378708e48e8e8f5f18642fb2a07fa905b5a853
Виктория <@LAPTOP-CFFC9F7Q>
2021-11-22 14:48:53 +0300

    Добавьте файлы проекта.

---
+++ Lab 7.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длина ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            CalcKub(a);
+            Console.ReadKey();
+        }
+        static void CalcKub(double a)
+        {
+            double v = a * a * a;
+            double s = 6 * a * a;
+            Console.WriteLine("Объем {0}",v);
+            Console.WriteLine("Площадь поверхности {0}",s);
+        }
+    }
+}
---
+++ Lab 7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длины сторон первого треугольника");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Длины сторон второго треугольника");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            CalcArea(a, b, c, x, y, z);
+            Console.ReadKey();
+        }
+        static void CalcArea (double a,double b, double c, double x, double y, double z)
+        {
+            double p1 = (a + b + c) / 2;
+            double p2 = (x + y + z) / 2;
+            double s1 = Math.Sqrt (p1 * (p1 - a) * (p1 - b) * (p1 - c));
+            double s2 = Math.Sqrt(p2 * (p2 - x) * (p2 - y) * (p2 - z));
+            if(s1>s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            
+        }
+    }
+}
